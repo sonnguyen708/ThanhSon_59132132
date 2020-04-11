660e986ce1e641f7107e3260bc923a2368bbe5ef
Admin <Admin@192.168.1.2>
2020-04-11 17:58:29 +0700

    MAU THIET KE

---
+++ src/interfaces/IQLDS.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author Admin
+ */
+import Baitap1.CaNhan;
+public interface IQLDS {
+    public abstract int them(CaNhan cn);
+    public abstract int xoa(String ten);
+    public abstract void inDS();
+}
